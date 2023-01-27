e7abe977e12a864c1e38d08d6138d6b6efc4deaa
seulGit <seulgi9421@gmail.com>
2023-01-27 16:27:41 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb+srv://seulki:1234@boilerplate.vx1luhi.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!안녕안녕안ㄴ여'))
+app.get('/', (req, res) => res.send('Hello World!안녕안녕안녕!!!'))
 
+
+app.post('/register', (req, res) => {
+    // 회원 가입할떄 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
