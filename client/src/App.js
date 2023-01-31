d38ad4301b89b05c60deaae9008acf4e6f2f7789
seulGit <seulgi9421@gmail.com>
2023-01-31 15:16:27 +0900

    230131 라우터 작성 및 프록시 작업

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+
+        <hr />
+
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} /> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
